f9cf3531f29c5fb5e9cde1703163867d23a87a87
Hannesbrm <hornunghannes@gmail.com>
2025-07-31 10:35:43 +0200

    Revert "Merge pull request #45 from Hannesbrm/codex/add-mcp9600-sensor-reader-class"
    This reverts commit b8518e3244a5e820de7d990384fbd3fe626c57d7, reversing
    changes made to 022e7f1554bec71af5485aca51657bca1f950ade.

--- fan_control_project/config/__init__.py
+++ fan_control_project/config/__init__.py
@@ -1,4 +1,3 @@
 from .config_manager import load_config, save_config
-from .hardware_config import load_hardware_config
 
-__all__ = ["load_config", "save_config", "load_hardware_config"]
+__all__ = ["load_config", "save_config"]
